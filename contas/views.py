f9e8ad05b8ce9d2bbd89c52aa4da42d42661be95
mysantos <mylenaspmendes@gmail.com>
2019-07-25 12:10:04 -0300

    estrutura formularios

---
+++ contas/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Pessoa
+
+# Create your views here.
+    
+def mostrar_formulario(request):
+    if request.method == 'POST':
+        pessoa = Pessoa()
+        pessoa.nome = request.POST ['nome']
+        pessoa.cpf = request.POST ['cpf']
+        pessoa.save ()
+
+    return render(request, 'index.html')
+
+def mostrar_pessoas(request):
+        pessoas = Pessoa.objects.all()
+
+        return render(request, 'pessoas.html', {'dados': pessoas})
+
